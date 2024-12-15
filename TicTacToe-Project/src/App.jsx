e1eaad920333c1d1b1dc733c050a45886e8e8f18
kantlori <atabeywade@gmail.com>
2024-12-15 10:51:10 +0300

    small edit

--- TicTacToe-Project/src/App.jsx
+++ TicTacToe-Project/src/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { useState } from 'react';
 import Square from './Square';
+import calculateWinner from './Calculater';
+
 
 
 function Board() {
@@ -62,24 +64,5 @@ function Board() {
 
 }
 
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-  ];
-  for (let line of lines) {
-    const [a, b, c] = line
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a]
-    }
-  }
-  return null;
-}
 
 export default Board
